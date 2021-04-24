48b67994e1bb9190658203914e96088ca64f9487
KamiMoon <erickizaki@gmail.com>
2021-04-24 11:45:36 -0500

    basic auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { Provider } from "next-auth/client";
 import "../client/styles/global.css";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
--- pages/wedding/guests.tsx
+++ pages/wedding/guests.tsx
@@ -1,3 +1,7 @@
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/client";
+import AccessDenied from "../../client/auth/access-denied";
+
 import { GetStaticProps } from "next";
 import { sampleUserData } from "../../server/data/sample-data";
 import { User } from "../../shared/models/User";
@@ -9,11 +13,41 @@ type Props = {
   items: User[];
 };
 
-const GuestListPage = ({ items }: Props) => (
-  <Layout title="Guest List">
-    <GuestList guests={items} />
-  </Layout>
-);
+function GuestListPage({ items }: Props) {
+  const [session, loading] = useSession();
+  const [content, setContent] = useState();
+
+  // Fetch content from protected route
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/examples/protected");
+      const json = await res.json();
+      if (json.content) {
+        setContent(json.content);
+      }
+    };
+    fetchData();
+  }, [session]);
+
+  // When rendering client side don't display anything until loading is complete
+  //TODO: errors in console with react-dom.development.js?61bb:67 Warning: Did not expect server HTML to contain a <div> in <div>.
+  //if (typeof window !== "undefined" && loading) return null;
+
+  // If no session exists, display access denied message
+  if (!session) {
+    return (
+      <Layout>
+        <AccessDenied />
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout title="Guest List">
+      <GuestList guests={items} />
+    </Layout>
+  );
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   // Example for including static props in a Next.js function component page.
